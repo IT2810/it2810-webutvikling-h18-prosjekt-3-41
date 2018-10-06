eef2a2ee74c83271be2c348c4f69c16a30bfd0eb
kristoffergjerde <kristoffergjerde@gmail.com>
2018-10-06 23:04:43 +0200

    #7 Added the calendar to CalendarScreen
    o Used react-native-calendars to add a new Calendar component
    o I think we should make a new screen AgendaScreen, that we show each time we press a date in the Calendar. This is used in examples in the github repo for react-native-calendars
    o Added the theme settings, so we all can see how we easily can change them.
    o Changed the arrow icons in the calendar to our Expo icons

--- screens/CalenderScreen.js
+++ screens/CalenderScreen.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import { Text, View } from 'react-native';
 import {Ionicons} from "@expo/vector-icons";
+import { Calendar, CalendarList, Agenda} from 'react-native-calendars';
+
 
 export default class CalenderScreen extends Component {
 
@@ -12,10 +14,59 @@ export default class CalenderScreen extends Component {
         const { navigate } = this.props.navigation;
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>
-                    CalenderScreen
-                </Text>
-                <Ionicons name={"md-calendar"} size={32} color="black" />
+                <Calendar
+                    // Initially visible month. Default = Date()
+                    current={new Date()}
+                    // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+                    minDate={'2016-05-10'}
+                    // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+                    maxDate={'2020-05-30'}
+                    // Handler which gets executed on day press. Default = undefined
+                    onDayPress={(day) => {console.log('selected day', day)}}
+                    // Handler which gets executed on day long press. Default = undefined
+                    onDayLongPress={(day) => {console.log('selected day', day)}}
+                    // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+                    monthFormat={'dd MM yyyy'}
+                    // Handler which gets executed when visible month changes in calendar. Default = undefined
+                    onMonthChange={(month) => {console.log('month changed', month)}}
+                    // Replace default arrows with custom ones (direction can be 'left' or 'right')
+                    renderArrow={(direction) => (
+                        direction === "left" ? <Ionicons name={"md-arrow-dropleft"}/> : <Ionicons name={"md-arrow-dropright"}/> )}
+                    // Do not show days of other months in month page. Default = false
+                    hideExtraDays={true}
+                    // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
+                    // day from another month that is visible in calendar page. Default = false
+                    disableMonthChange={true}
+                    // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+                    firstDay={1}
+                    // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+                    onPressArrowLeft={substractMonth => substractMonth()}
+                    // Handler which gets executed when press arrow icon left. It receive a callback can go next month
+                    onPressArrowRight={addMonth => addMonth()}
+                    // Specify style for calendar container element. Default = {}
+                    style={{
+                        width : "100%"
+                    }}
+                    // Specify theme properties to override specific styles for calendar parts. Default = {}
+                    theme={{
+                        backgroundColor: '#ffffff',
+                        calendarBackground: '#ffffff',
+                        textSectionTitleColor: '#b6c1cd',
+                        selectedDayBackgroundColor: '#00adf5',
+                        selectedDayTextColor: '#ffffff',
+                        todayTextColor: '#00adf5',
+                        dayTextColor: '#2d4150',
+                        textDisabledColor: '#d9e1e8',
+                        dotColor: '#00adf5',
+                        selectedDotColor: '#ffffff',
+                        arrowColor: 'orange',
+                        monthTextColor: 'blue',
+                        textMonthFontWeight: 'bold',
+                        textDayFontSize: 16,
+                        textMonthFontSize: 16,
+                        textDayHeaderFontSize: 12
+                    }}
+                />
             </View>
         );
     }
