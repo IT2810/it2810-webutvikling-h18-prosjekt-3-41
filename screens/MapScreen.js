5b098081b0b3ffa2b9faa495be742fd36f8a27c1
martinjoha <martinjohansen1705@gmail.com>
2018-10-07 19:34:01 +0200

    #3 Added my current location when opening the application. The marker will eventually be centered better around the location of the user #5 Can now search for specific users by their name.

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, ActivityIndicator, TextInput } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import MapView, { Marker } from 'react-native-maps';
 import _ from 'lodash';
@@ -15,56 +15,111 @@ export default class MapScreen extends Component {
       this.state = {
           brothers: {
               1: {
-                  brother: 'General lover',
+                  name: 'General lover',
                   latlng: {
                       latitude: 63.4157,
                       longitude: 10.4061
                   }
               },
               2: {
-                  brother: 'Skruf lover',
+                  name: 'Skruf lover',
+                  latlng:{
+                      latitude: 63.43,
+                      longitude: 10.46
+                  }
+              },
+              3: {
+                  name: 'Georg',
+                  latlng:{
+                      latitude: 63.4,
+                      longitude: 10.49
+                  }
+              },
+              4: {
+                  name: '02',
                   latlng:{
                       latitude: 63.43,
                       longitude: 10.5
                   }
               }
-          }
-      };
+          },
+          myposition: {
+              latitude: null,
+              longitude: null
+          },
+          searchterm: ''
+      }
     }
 
+
     onRegionChange(region) {
-    this.setState({
-      region
-    });
+        this.setState({
+          region
+        });
     }
+
 
     renderMarkers() { 
         return _.map(this.state.brothers, id => {
+            if(id.name.includes(this.state.searchterm)) {
+                return(
+                    <Marker key={id.name}
+                            coordinate={id.latlng}
+                            title={id.name}/>
+                )
+            }
+        });
+    }
 
+    renderMap(){
+        if((!this.state.myposition.latitude)||(!this.state.myposition.longitude)){
             return(
-                <Marker key={id.brother}
-                    coordinate={id.latlng}
-                    title={id.brother}/>
-            )});
+                <ActivityIndicator size="large" color="#0000ff"/>
+            );
+        }
+        else {
+            return(
+                <MapView style={styles.map}
+                         initialRegion={{
+                             latitude: this.state.myposition.latitude,
+                             longitude: this.state.myposition.longitude,
+                             latitudeDelta: 0.0922,
+                             longitudeDelta: 0.0421,
+                         }}
+                         region ={this.state.region}
+                         onRegionChangeComplete={this.onRegionChange.bind(this)}
+                         rotateEnabled={false}>
+                    {this.renderMarkers()}
+                    <Marker image={require('../assets/android-locate.png')}
+                            coordinate={this.state.myposition}
+                            title={"Me"}/>
+                </MapView>
+            )
+        }
+    }
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({
+                    myposition: {
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude
+                    },
+                });
+            },
+            error => (console.log(error)));
     }
 
 
+
+
+
     render() {
-        const { navigate } = this.props.navigation;
+        const { navigfates } = this.props.navigation;
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-              <MapView style={styles.map}
-                        initialRegion={{
-                          latitude: 63.418546,
-                          longitude: 10.402860,
-                          latitudeDelta: 0.0922,
-                          longitudeDelta: 0.0421,
-                        }}
-                        region ={this.state.region}
-                        onRegionChangeComplete={this.onRegionChange.bind(this)}
-                        rotateEnabled={false}>
-                  {this.renderMarkers()}
-              </MapView>
+                {this.renderMap()}
             </View>
         );
     }
@@ -72,6 +127,7 @@ export default class MapScreen extends Component {
 
 const styles = StyleSheet.create({
   map: {
+    zIndex: 1,
     position: 'absolute',
     top: 0,
     left: 0,
@@ -79,3 +135,5 @@ const styles = StyleSheet.create({
     bottom: 0,
   },
 });
+
+
