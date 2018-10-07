10b7ae507d5f9c0c7cfad8ec5fdec556bfc1dbe0
Oscar Vik <OscarVik@dhcp-10-22-12-92.wlan.ntnu.no>
2018-10-07 14:34:00 +0200

    #2 implemented react-native-maps and added MapView to MapScreen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import MapView from 'react-native-maps';
 
 export default class MapScreen extends Component {
 
@@ -8,18 +9,46 @@ export default class MapScreen extends Component {
         title: 'Map',
     };
 
+    constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+      }
+
+    onRegionChange(region) {
+    this.setState({
+      region
+    });
+    }
 
 
     render() {
         const { navigate } = this.props.navigation;
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>
-                    MAPSCREEN
-                </Text>
-                <Ionicons name="md-map" size={32} color="black" />
+              <MapView style={styles.map}
+                        initialRegion={{
+                          latitude: 63.418546,
+                          longitude: 10.402860,
+                          latitudeDelta: 0.0922,
+                          longitudeDelta: 0.0421,
+                        }}
+                        region ={this.state.region}
+                        onRegionChangeComplete={this.onRegionChange.bind(this)}
+                        rotateEnabled={false}
+                />
             </View>
         );
     }
 }
 
+const styles = StyleSheet.create({
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
